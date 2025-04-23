48bd13c9add11ef12d0cc187a9f891b453af634e
god4120 <49931976+god4120@users.noreply.github.com>
2025-04-23 17:40:50 -0400

    Update riotAPIcalls.py

--- riotAPIcalls.py
+++ riotAPIcalls.py
@@ -8,6 +8,7 @@ import os
 import requests
 import re
 from pathlib import Path
+import champions
 
 dotenv_path = Path('secrets.env')
 load_dotenv(dotenv_path=dotenv_path)
@@ -17,7 +18,13 @@ auth = os.getenv("Riot_Api_Key")
 
 
 def parsepuuid(gamename, tag):
+    """
+    Parses the players PUUID from account info
+    Args: 2 strings gamename and a 3 letter tagline
+    return: A string type containing the players PUUID
+    """
     return account_by_riot_id(gamename, tag).text.split('"')[3]
+
 
 def account_by_riot_id(gamename, tagline):
     """
@@ -33,11 +40,54 @@ def account_by_riot_id(gamename, tagline):
 
 
 # Champion-Mastery-V4:
-def topchamp(puuid, count=1):
+def champsplayed(puuid):
+    """
+       Provides a list of all champs played and their level
+       Args: A string puuid
+       return: An array of strings champs played and their level
+       """
+    url = "https://na1.api.riotgames.com"
+    endpoint = ("/lol/champion-mastery/v4/champion-masteries/by-puuid/" + puuid + "/"
+                + "?api_key=" + auth)
+    response = requests.get(url + endpoint)
 
-    response = 5
-    return response
+    response = response.text
+    response = re.split(r'[;,:"\s]+', response)
+    champs = []
+    level = []
+    played = []
+
+    for index, item in enumerate(response):
+        spot = index
+        if response[spot] == 'championId':
+            try:
+                champs.append(champions.champion_Names[int(response[spot+1])])
+                level.append(response[spot+3])
+            except KeyError:
+                print('KeyError:' + response[spot+1] + 'not found')
+    combo = 'Name: Level'
+    played.append(combo)
+    for champ in champs:
+        combo = champ + ': ' + level[champs.index(champ)]
+        played.append(combo)
+
+    return played
+
+
+def top3(puuid):
+    """
+    Provides a string of the top 3 champs played and their level
+    args: A string puuid
+    return: A string type containing the top 3 champs played and their level
+    """
+    top3 = ''
+    i =0
+    while i < 3:
+        top3 += champsplayed(puuid)[i] + '\n'
+        i += 1
+    return top3
 
 
 if __name__ == "__main__":
-    print (parsepuuid("tegotrick", "ttv"))
+    print(champsplayed(parsepuuid('dux ossa', 'Famly')))
+    print(top3(parsepuuid('dux ossa', 'Famly')))
